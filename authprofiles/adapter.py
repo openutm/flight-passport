913d00317e1eff7473dd5c759bf78e7efd92b63f
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2025-03-18 17:51:23 +0000

    Updated to remove tld dependency

--- authprofiles/adapter.py
+++ authprofiles/adapter.py
@@ -1,6 +1,5 @@
 from os import environ as env
 
-import tldextract
 from allauth.account.adapter import DefaultAccountAdapter
 from django.forms import ValidationError
 from dotenv import find_dotenv, load_dotenv
@@ -18,10 +17,7 @@ class PassportAccountAdapter(DefaultAccountAdapter):
         domain = email.split("@")[1]
         white_listed_domain = [i for i in DOMAIN_WHITELIST.split(";")]
 
-        if domain in white_listed_domain:
-            ext = tldextract.extract(domain)
-            tld = ext.domain.upper()
-            suffix = ext.suffix.upper()
+        if domain in white_listed_domain:            
             return email
         else:
             raise ValidationError("You are restricted from registering. Please contact admin.")
